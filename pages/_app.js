8c3fa0d71ad18eb00e71b0bef6c6a8d5f70ddaca
Rasheed <rashid0241@gmail.com>
2022-04-25 12:02:37 +0300

    second

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import React from 'react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
+
+
+
